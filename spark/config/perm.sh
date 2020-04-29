df99fc13d2ea596b28f78abfad2c4c416b3ac308
Nithin Rao <nramarao@hawk.iit.edu>
2020-04-29 00:53:23 -0500

    Added Perm Script

--- spark/config/perm.sh
+++ spark/config/perm.sh
@@ -1,4 +1,6 @@
 chown hadoop /home/hadoop/sf/sort
 chown hadoop /home/hadoop/sf/sort/gensort
 chown hadoop /home/hadoop/sf/sort/valsort
+chmod +x run.sh
+su hadoop
 hadoop fs -mkdir -p /user/hadoop/input

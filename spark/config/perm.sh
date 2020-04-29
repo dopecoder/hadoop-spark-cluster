04518e347077b2e9a9f1b8f0ca87542295080ba2
Nithin Rao <nramarao@hawk.iit.edu>
2020-04-29 00:48:07 -0500

    Added Perm Script

---
+++ spark/config/perm.sh
@@ -0,0 +1,4 @@
+chown hadoop /home/hadoop/sf/sort
+chown hadoop /home/hadoop/sf/sort/gensort
+chown hadoop /home/hadoop/sf/sort/valsort
+hadoop fs -mkdir -p /user/hadoop/input
--- spark/config/run.sh
+++ spark/config/run.sh
@@ -1,10 +1,11 @@
 for file_size in 1 4 16 64
 do
     total_file_size=$(( file_size*1024*1024*1024 / 100 ))
-    ~/sf/sort/gensort -a ${file_size} input.txt
+    cd /sf/sort/
+    ./gensort -a ${file_size} input.txt
     hadoop fs -put input.txt /user/hadoop/input
     rm input.txt
-    hadoop -jar /home/hadoop/sf/HadoopSort/target/HadoopSort-*.jar /user/hadoop/input /user/hadoop/output >> ~/sf/${file_size}-GB-logs.txt
+    hadoop jar /home/hadoop/sf/HadoopSort/target/HadoopSort-*.jar /user/hadoop/input /user/hadoop/output >> ~/sf/${file_size}-GB-logs.txt
     hadoop fs -rm /user/hadoop/input/input.txt
     hadoop fs -rm -r -f /user/hadoop/output
 done

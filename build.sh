99f67441b05762d2c304529a9eeda4bace5c6c2c
Nithin Rao <nramarao@hawk.iit.edu>
2020-04-29 20:26:47 -0500

    more scripts

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 
-if [[ $# -lt 1 ]]; then
+if [[ $# -lt 1 ]]
+then
     echo "Please mention the config : 1-Large 1-Small 4-Small"
     exit
 fi
 
 
-if [[ $# -eq 2 ]]; then
+if [[ $# -eq 2 ]]
+then
     ./cluster-${2}.sh stop
 fi
 

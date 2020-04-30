d27deb850195af0969c0703511fd0a374f3d9879
Nithin Rao <nramarao@hawk.iit.edu>
2020-04-29 20:19:32 -0500

    more scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,21 @@
+
+if [[ $# -lt 1 ]]; then
+do
+    echo "Please mention the config : 1-Large 1-Small 4-Small"
+    exit
+done
+
+
+if [[ $# -eq 2 ]]; then
+do
+    ./cluster-${2}.sh stop
+done
+
+cp workers-${1} spark/config/workers
+
 cd scalabase
 ./build.sh
 cd ../spark
 ./build.sh
 cd ..
-./cluster.sh deploy
+./cluster-${1}.sh deploy
